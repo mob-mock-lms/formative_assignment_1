ee9df997e9bfeef65c711312857e03e5240edf07
mutabazi-bruno <b.mutabazi@gmail.com>
2026-02-05 19:00:03 +0200

    feat: add assignment management system
    - Add Assignment model with priority levels and due dates
    - Add AssignmentsScreen with CRUD operations
    - Add form for creating/editing assignments
    - Integrate with QuizzesScreen
    - Features: sort, filter, mark complete, overdue detection

--- lib/screens/quizzes.dart
+++ lib/screens/quizzes.dart
@@ -1,4 +1,4 @@
-import 'package:assignments/widgets/app_app_bar.dart';
+import 'package:assignments/screens/assignments_screen.dart';
 import 'package:flutter/material.dart';
 
 class QuizzesScreen extends StatelessWidget {
@@ -6,13 +6,6 @@ class QuizzesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.transparent,
-      appBar: const PreferredSize(
-        preferredSize: Size.fromHeight(55),
-        child: AppAppBar(title: "Quizzes"),
-      ),
-      body: Placeholder(),
-    );
+    return const AssignmentsScreen();
   }
 }
